937494328a90ca5792d22d90a15d13c2cb75f18f
jademcosta <jademcosta@gmail.com>
2017-05-14 11:57:46 -0300

    Create SQLite database helper
    It leverages cupboard to help us create the tables.
    
    It has to be a singleton in order to avoid threading problems. More
    about it here: http://stackoverflow.com/a/8888606/1739502

---
+++ app/src/main/java/com/jademcosta/starstore/database/CupboardSQLiteOpenHelper.java
@@ -0,0 +1,43 @@
+package com.jademcosta.starstore.database;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.jademcosta.starstore.entity.Item;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+public class CupboardSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    private static CupboardSQLiteOpenHelper instance;
+
+    private static final String DATABASE_NAME = "cart.db";
+    private static final int DATABASE_VERSION = 1;
+
+    static {
+        cupboard().register(Item.class);
+    }
+
+    private CupboardSQLiteOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static CupboardSQLiteOpenHelper getInstance(Context context) {
+        if(instance == null) {
+            instance = new CupboardSQLiteOpenHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        cupboard().withDatabase(db).createTables();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        cupboard().withDatabase(db).upgradeTables();
+    }
+}
