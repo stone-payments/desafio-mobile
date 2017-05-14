b66018983a5e442e4dd43bfb8eaed7fa3138a5ea
jademcosta <jademcosta@gmail.com>
2017-05-13 21:09:23 -0300

    Remove unused menu code

--- app/src/main/java/com/jademcosta/starstore/itemsList/ItemsListActivity.java
+++ app/src/main/java/com/jademcosta/starstore/itemsList/ItemsListActivity.java
@@ -6,8 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -35,28 +33,6 @@ public class ItemsListActivity extends AppCompatActivity implements ItemsListCon
         initializeListeners();
 
         presenter.onCreate();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_items_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
