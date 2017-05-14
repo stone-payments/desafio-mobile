89e7db6a8510a2ea59ee829cc3e4ad23d04f3ee2
jademcosta <jademcosta@gmail.com>
2017-05-13 23:24:51 -0300

    Add layout manager to recyclerview

--- app/src/main/java/com/jademcosta/starstore/itemsList/ItemsListActivity.java
+++ app/src/main/java/com/jademcosta/starstore/itemsList/ItemsListActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -82,5 +83,8 @@ public class ItemsListActivity extends AppCompatActivity implements ItemsListCon
         fab = (FloatingActionButton) findViewById(R.id.fab);
         recyclerView = (RecyclerView) findViewById(R.id.activity_items_list_recyclerview);
         loadingView = (ProgressBar) findViewById(R.id.activity_items_list_loading_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
