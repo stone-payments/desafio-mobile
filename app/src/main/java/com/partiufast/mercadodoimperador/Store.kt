5b6aaade51dbaf808a0b8ba052ce1e3719848f48
Miguel <mfsousa@poli.ufrj.br>
2017-05-14 03:37:51 -0300

    getting data from URL

---
+++ app/src/main/java/com/partiufast/mercadodoimperador/Store.kt
@@ -0,0 +1,45 @@
+package com.partiufast.mercadodoimperador
+
+
+import java.util.*
+
+class Store {
+    private val cartProducts: ArrayList<Product> = ArrayList<Product>()
+    private val availableProducts: ArrayList<Product> = ArrayList<Product>()
+
+    fun setAvailableProducts(products: ArrayList<Product>) {
+        availableProducts.addAll(products)
+    }
+
+    fun addProductToCart(product: Product) {
+        if (cartProducts.size == 0)
+            cartProducts.add(product)
+        else {
+            if (getProductIndexIfContains(product) == -1)
+                cartProducts.add(product)
+        }
+    }
+
+    private fun getProductIndexIfContains(product: Product): Int {
+        for (i in cartProducts.indices) {
+            if (cartProducts[i].title == (product.title)) {
+                cartProducts[i].productCount = product.productCount
+                return i
+            }
+        }
+        return -1
+    }
+
+
+    fun getAvailableProducts(): ArrayList<Product> {
+        return availableProducts
+    }
+
+    fun getCartProducts(): ArrayList<Product> {
+        return cartProducts
+    }
+
+
+}
+
+
--- app/src/main/java/com/partiufast/mercadodowatto/Product.kt
+++
@@ -1,7 +0,0 @@
-package com.partiufast.mercadodowatto
-
-import java.math.BigDecimal
-
-class Product(val title: String, val price: BigDecimal, val seller: String, val thumbnailLink: String){
-
-}
