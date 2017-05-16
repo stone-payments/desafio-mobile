4b4fffd51a6531a6499b9138c8229eec5125dc53
jademcosta <jademcosta@gmail.com>
2017-05-15 23:10:35 -0300

    Create XML for transactions list screen

--- app/src/main/java/com/jademcosta/starstore/transactionsList/TransactionsListActivity.java
+++ app/src/main/java/com/jademcosta/starstore/transactionsList/TransactionsListActivity.java
@@ -3,23 +3,40 @@ package com.jademcosta.starstore.transactionsList;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.jademcosta.starstore.R;
-import com.jademcosta.starstore.creditCard.CreditCardInjector;
 
 public class TransactionsListActivity extends AppCompatActivity
         implements TransactionsListContract.View {
 
     private Presenter presenter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit_card);
 
+        initializeViews();
+     
         TransactionsListInjector injector = new TransactionsListInjector();
         injector.inject(this, getApplicationContext());
     }
+
+    private void initializeViews() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.transactions_list_screen_title));
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.activity_transactions_list_recyclerview);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
 
     public void setPresenter(Presenter presenter) {
         this.presenter = presenter;
