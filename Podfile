59dcd43d9869c6b67135b69f21b1452fc3129884
Rafael de Paula <rafael.paula@ebix.com>
2017-08-22 00:10:33 -0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+use_frameworks!
+inhibit_all_warnings!
+
+def available_pods
+
+end
+
+target 'mobCommerce' do
+  available_pods
+end
---
+++ mobCommerce/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobCommerce
+//
+//  Created by Rafael de Paula on 21/08/17.
+//  Copyright © 2017 Rafael de Paula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
