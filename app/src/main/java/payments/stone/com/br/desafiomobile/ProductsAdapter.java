ff23c996969ce383a0c3766bace482cf73f4f1aa
William Gouvea <william.cruz.gouvea@gmail.com>
2017-09-19 04:14:24 -0300

    -Add Cart Activity and related Toolbar menu item -Add Splash Activity in order to do basic injections and also sync some data if needed

--- app/src/main/java/payments/stone/com/br/desafiomobile/ProductsAdapter.java
+++ app/src/main/java/payments/stone/com/br/desafiomobile/ProductsAdapter.java
@@ -81,12 +81,12 @@ class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ProductViewHo
         @Override
         public boolean onMenuItemClick(MenuItem menuItem) {
             switch (menuItem.getItemId()) {
-                case R.id.action_add_favourite:
-                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_play_next:
-                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
-                    return true;
+//                case R.id.action_add_favourite:
+//                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.action_play_next:
+//                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+//                    return true;
                 default:
             }
             return false;
