b39b8e05d385fe211a5662a9f2d524b8bae0cc26
William Gouvea <william.cruz.gouvea@gmail.com>
2017-09-21 21:52:32 -0300

    -Started deisgn of Shopping Cart screen -Change some colors and fonts -Add navigation to  Cart screen

--- app/src/main/java/payments/stone/com/br/desafiomobile/CartActivity.java
+++ app/src/main/java/payments/stone/com/br/desafiomobile/CartActivity.java
@@ -1,7 +1,12 @@
 package payments.stone.com.br.desafiomobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -9,5 +14,55 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cart_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_product, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_show_cart:
+                startActivity(new Intent(this, CartActivity.class));
+                return true;
+
+
+            case android.R.id.home:
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem search = menu.findItem(R.id.action_search);
+        if (search != null) {
+            search.setVisible(false);
+        }
+
+        MenuItem cart = menu.findItem(R.id.action_show_cart);
+        if (cart != null) {
+            cart.setVisible(false);
+        }
+
+        return true;
     }
 }
