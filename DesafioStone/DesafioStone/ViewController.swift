c44eeecbeec392176c603280ea1cc8062be10462
Luan Kalume <luanbarbalho@hotmail.com>
2017-09-26 13:42:24 -0300

    First commit.

---
+++ DesafioStone/DesafioStone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesafioStone
+//
+//  Created by Luan Barbalho Kalume on 26/09/17.
+//  Copyright © 2017 Luan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
