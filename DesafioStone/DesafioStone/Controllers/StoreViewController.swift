fcd28ca17ba5e590355216c2aaead09fdeac1f7c
Luan Kalume <luanbarbalho@hotmail.com>
2017-09-26 16:22:58 -0300

    Started implementation of StoreScreen.

--- DesafioStone/DesafioStone/Controllers/StoreViewController.swift
+++ DesafioStone/DesafioStone/Controllers/StoreViewController.swift
@@ -9,27 +9,35 @@
 import UIKit
 
 class StoreViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    self.refreshProductList()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func refreshProductList() {
+    
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
---
+++ DesafioStone/DesafioStone/Models/ProductModel.swift
@@ -0,0 +1,19 @@
+//
+//  ProductModel.swift
+//  DesafioStone
+//
+//  Created by Luan Barbalho Kalume on 26/09/17.
+//  Copyright © 2017 Luan. All rights reserved.
+//
+
+import Foundation
+
+struct ProductModel {
+  
+  let title: String
+  let price: Double
+  let zipcode: String
+  let thumbnailHd: String
+  let date: String
+  
+}
