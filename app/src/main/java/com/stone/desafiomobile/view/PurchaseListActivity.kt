296aaacd4cda7c37628f5d4ac4614c669589be5d
Renan Barros <renanbarrosrbs@gmail.com>
2017-10-14 23:53:51 -0300

    Navegação no botão para cima

--- app/src/main/java/com/stone/desafiomobile/view/PurchaseListActivity.kt
+++ app/src/main/java/com/stone/desafiomobile/view/PurchaseListActivity.kt
@@ -9,5 +9,6 @@ class PurchaseListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purchase_list)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
