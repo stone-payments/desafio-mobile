1dd79a3e3802b90f84d138416f793053e39e99e1
Renan Barros <renanbarrosrbs@gmail.com>
2017-10-16 01:11:04 -0200

    Comentarios das classes de utilidade

--- app/src/main/java/com/stone/desafiomobile/utils/CurrencyFormat.kt
+++ app/src/main/java/com/stone/desafiomobile/utils/CurrencyFormat.kt
@@ -1,10 +1,17 @@
 package com.stone.desafiomobile.utils
 
+/**
+ * Metodo para formatar moedas
+ * @return moeda formatada para Real
+ */
 fun Long.formatPriceReal(): String {
     val stringValue = this.toString()
+    // Ultimos dois digitos como centavos
     val cents = stringValue.takeLast(2)
+    // resto dos digitos
     var amount = stringValue.substring(0, stringValue.lastIndex - 1)
 
+    // impede que fique mal formatado quando o valor for somente centavos ex: R$,50
     if (amount == "") {
         amount = "0"
     }
--- app/src/main/java/com/stone/desafiomobile/utils/FormValidation.kt
+++ app/src/main/java/com/stone/desafiomobile/utils/FormValidation.kt
@@ -3,6 +3,10 @@ package com.stone.desafiomobile.utils
 import android.widget.EditText
 import com.stone.desafiomobile.R
 
+/**
+ * valida se o input esta vazio
+ * @return objeto para continuar mais validações ou nulo se a validação falhou
+ */
 fun EditText.validateEmpty(): EditText? {
     if (this.text.isEmpty()) {
         this.setError(resources.getString(R.string.empty_error_text))
@@ -12,6 +16,11 @@ fun EditText.validateEmpty(): EditText? {
     return this
 }
 
+/**
+ * valida se o input tem o tamanho necessario
+ * @param minLength valor minimo que o input deve possuir
+ * @return objeto para continuar mais validações ou nulo se a validação falhou
+ */
 fun EditText.validateMinLength(minLength: Int): EditText? {
     if (this.text.length < minLength) {
         val message = String.format(resources.getString(R.string.min_length_error_text), minLength);
@@ -22,7 +31,10 @@ fun EditText.validateMinLength(minLength: Int): EditText? {
     return this
 }
 
-
+/**
+ * valida se o input segue o padrão MM/yy
+ * @return objeto para continuar mais validações ou nulo se a validação falhou
+ */
 fun EditText.validateDate(): EditText? {
     if (!this.text.matches(Regex("[0-1][0-9]/[0-9][0-9]")) || this.text.take(2).toString().toLong() > 12) {
         val message = resources.getString(R.string.invalid_format_error_text);
