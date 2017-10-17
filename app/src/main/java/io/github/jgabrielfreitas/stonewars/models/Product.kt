df4735e876975519a83af67b78ffa4ff0665eb5f
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-10-16 23:25:35 -0200

    initial commit

---
+++ app/src/main/java/io/github/jgabrielfreitas/stonewars/models/Product.kt
@@ -0,0 +1,11 @@
+package io.github.jgabrielfreitas.stonewars.models
+
+/**
+ * Created by JGabrielFreitas on 16/10/2017.
+ */
+data class Product(var title       : String,
+                   var price       : Int,
+                   var zipcode     : String,
+                   var seller      : String,
+                   var thumbnailHd : String,
+                   var date        : String)
