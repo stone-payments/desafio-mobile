a5bf335e93e31d9b5af3911ebb94fba17379ce0c
Kennedy Noia <kennedynoia@gmail.com>
2017-11-27 18:06:51 -0300

    Add cocoapods with alamofire, alamofireimage and realm alamoframeworks

---
+++ Stone Store Star/Pods/Target Support Files/Pods-Stone Store Star/Pods-Stone Store Star-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stone_Store_Star : NSObject
+@end
+@implementation PodsDummy_Pods_Stone_Store_Star
+@end
---
+++ Stone Store Star/Pods/Target Support Files/Pods-Stone Store StarUITests/Pods-Stone Store StarUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stone_Store_StarUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Stone_Store_StarUITests
+@end
