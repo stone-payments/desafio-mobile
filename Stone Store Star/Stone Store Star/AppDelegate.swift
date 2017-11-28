59e6ccf0563d09f8359a43838f0b55e22c4f07df
Kennedy Noia <kennedynoia@gmail.com>
2017-11-27 22:12:32 -0300

    Add load realm call to verify and charge first data with seed

--- Stone Store Star/Stone Store Star/AppDelegate.swift
+++ Stone Store Star/Stone Store Star/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        openRealm()
         return true
+    }
+    
+    func openRealm() {
+        let seed = RSeed()
+        seed.run()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
