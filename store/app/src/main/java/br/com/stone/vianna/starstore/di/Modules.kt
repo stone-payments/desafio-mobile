13f55c13e0f6dfb7abed3e4ccd7d6e37d2104c15
Eduardo Vianna <>
2019-05-09 22:33:41 -0300

    setup retrofit DI

--- store/app/src/main/java/br/com/stone/vianna/starstore/di/Modules.kt
+++ store/app/src/main/java/br/com/stone/vianna/starstore/di/Modules.kt
@@ -1,9 +1,41 @@
 package br.com.stone.cryptowallet.di
 
+import android.arch.persistence.room.Room
 import br.com.stone.vianna.starstore.AppDatabase
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val applicationModule = module(override = true) {
+
+    single { createOkHttpClient() }
+    single { Room.databaseBuilder(get(), AppDatabase::class.java, "store_database").build() }
+}
 
 val daoModule = module {
     single { get<AppDatabase>().itemDao() }
     single { get<AppDatabase>().transactionDao() }
+}
+
+fun createOkHttpClient(): OkHttpClient {
+
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+    return OkHttpClient.Builder()
+            .connectTimeout(60L, TimeUnit.SECONDS)
+            .readTimeout(60L, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor).build()
+}
+
+inline fun <reified T> createWebService(okHttpClient: OkHttpClient, url: String): T {
+    val retrofit = Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
+    return retrofit.create(T::class.java)
 }
